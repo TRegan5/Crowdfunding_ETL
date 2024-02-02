4a622c9814855d2aa289a54a78a65779bc2ec63a
TRegan5 <tpregan5@gmail.com>
2024-02-02 16:36:51 -0500

    Updated readme and uploaded database ERD image and schema

---
+++ Database/crowdfunding_db_schema.sql
@@ -0,0 +1,60 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/D8IgWK
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "contacts" (
+    "contact_id" INTEGER   NULL,
+    "first_name" VARCHAR(50)   NULL,
+    "last_name" VARCHAR(50)   NULL,
+    "email" VARCHAR(50)   NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" VARCHAR   NULL,
+    "category" VARCHAR(50)   NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR   NULL,
+    "subcategory" VARCHAR(50)   NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "campaign" (
+    "cf_id" INTEGER   NULL,
+    "contact_id" INTEGER   NULL,
+    "company_name" VARCHAR(50)   NULL,
+    "description" TEXT   NULL,
+    "goal" FLOAT   NULL,
+    "pledged" FLOAT   NULL,
+    "outcome" VARCHAR(50)   NULL,
+    "backers_count" INTEGER   NULL,
+    "country" VARCHAR(50)   NULL,
+    "currency" VARCHAR(50)   NULL,
+    "launched_date" DATE   NULL,
+    "end_date" DATE   NULL,
+    "category_id" VARCHAR   NULL,
+    "subcategory_id" VARCHAR   NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+ALTER TABLE "contacts" ADD CONSTRAINT "fk_contacts_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "campaign" ("contact_id");
+
+ALTER TABLE "category" ADD CONSTRAINT "fk_category_category_id" FOREIGN KEY("category_id")
+REFERENCES "campaign" ("category_id");
+
+ALTER TABLE "subcategory" ADD CONSTRAINT "fk_subcategory_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "campaign" ("subcategory_id");
+
--- README.md
+++ README.md
@@ -3,7 +3,12 @@
 The majority of work was able to be completed based on instrucitonal slides, notes, and in class activities. 
 
 Some assistance came from online searches:
+
 -How extract a dictionary stored within a string? --> use json.loads() - https://www.w3schools.com/python/python_json.asp
+
 -Merging multiple dataframes - https://sparkbyexamples.com/pandas/pandas-merge-multiple-dataframes
 
-Special thanks to our TA Rhythm Ahir for helping troubleshoot uploads of csv files to our SQL database in pgAdmin!!!
+Special thanks to our TA Rhythm Ahir for helping troubleshoot a new PGadmin install and subsequent uploads of csv files to our SQL database in pgAdmin!!!
+
+All csv files were exported and stroed in the Resources folder under this project.
+The database ERD (created at https://app.quickdatabasediagrams.com/#/d/D8IgWK) and exported schema (crowdfunding_db_schema.sql) are stored separately in the Database folder. 
